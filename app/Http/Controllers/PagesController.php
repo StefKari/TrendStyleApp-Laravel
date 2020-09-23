47714828cfbf9932e013966842c669abd6ee06ad
StefKari <59152638+StefKari@users.noreply.github.com>
2020-09-23 23:52:00 +0200

    trendstyleapp

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,18 +6,42 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
+  /**
+   * Display a index page.
+   *
+   * @param void
+   * @return View
+   */
     public function index() {
       return view('pages.index');
     }
 
+    /**
+     * Display a o-nama page.
+     *
+     * @param void
+     * @return View
+     */
     public function onama() {
       return view('pages.o-nama');
     }
 
+    /**
+     * Display a cene page.
+     *
+     * @param void
+     * @return View
+     */
     public function cene() {
       return view('pages.cene');
     }
 
+    /**
+     * Display a politika_privatnosti page.
+     *
+     * @param void
+     * @return View
+     */
     public function politika_privatnosti() {
       return view('pages.politika-privatnosti');
     }
