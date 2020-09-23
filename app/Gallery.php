95aa04257f780148c955f18e87e61f9566f268fe
StefKari <59152638+StefKari@users.noreply.github.com>
2020-09-23 23:48:58 +0200

    trendstyleapp

--- app/Gallery.php
+++ app/Gallery.php
@@ -7,13 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array
+   */
+
   protected $fillable = [
       'gallery',
       'created_at',
       'updated_at',
       'user_id',
   ];
-
 
 
   public function user() {
